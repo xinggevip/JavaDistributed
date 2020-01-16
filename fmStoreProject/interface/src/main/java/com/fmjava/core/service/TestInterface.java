8a453ca8e2c937dbf5fea3fb226c2afad8fb8f88
xinggevip <1511844263@qq.com>
2020-01-16 16:00:26 +0800

    FM商城-Dao远程调用测试

--- fmStoreProject/interface/src/main/java/com/fmjava/core/service/TestInterface.java
+++ fmStoreProject/interface/src/main/java/com/fmjava/core/service/TestInterface.java
@@ -1,5 +1,9 @@
 package com.fmjava.core.service;
 
+import com.fmjava.core.pojo.good.Brand;
+
+import java.util.List;
+
 public interface TestInterface {
-    public String getName();
+    public List<Brand> getName();
 }
