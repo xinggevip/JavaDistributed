2332e089c598c17468bb6ed7783e1bf453da86a3
xinggevip <1511844263@qq.com>
2020-02-25 11:57:30 +0800

    FM商城-SpringSecurity自定义授权

--- fmStoreProject/web_shop/src/main/java/com/fmjava/core/service/UserDetailServiceImpl.java
+++ fmStoreProject/web_shop/src/main/java/com/fmjava/core/service/UserDetailServiceImpl.java
@@ -1,9 +1,14 @@
 package com.fmjava.core.service;
 
 import com.fmjava.core.pojo.seller.Seller;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.ArrayList;
 
 public class UserDetailServiceImpl implements UserDetailsService {
 
@@ -15,8 +20,23 @@ public class UserDetailServiceImpl implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        Seller one = sellerService.findOne(username);
-        System.out.println(one);
+        /* 先定义一个权限集合 */
+        ArrayList<GrantedAuthority> authList = new ArrayList<>();
+        authList.add(new SimpleGrantedAuthority("ROLE_SELLER"));
+
+        if (username == null){
+            return null;
+        }
+        // 到数据库中查询用户对象
+
+        Seller seller = sellerService.findOne(username);
+        if (seller != null){
+            if ("1".equals(seller.getStatus())){
+                // 说明用户已激活
+                return new User(username,"{noop}" + seller.getPassword(),authList);
+            }
+        }
+
         return null;
     }
 
