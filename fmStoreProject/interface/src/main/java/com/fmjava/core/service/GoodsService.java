c780e459bd36399e6ae1d599e7eba16d84bc7f2f
xinggevip <1511844263@qq.com>
2020-03-02 15:54:13 +0800

    FM商城-商家操作商品

---
+++ fmStoreProject/interface/src/main/java/com/fmjava/core/service/GoodsService.java
@@ -0,0 +1,20 @@
+package com.fmjava.core.service;
+
+import com.fmjava.core.pojo.entity.GoodsEntity;
+import com.fmjava.core.pojo.entity.PageResult;
+import com.fmjava.core.pojo.good.Goods;
+
+public interface GoodsService {
+    public void add(GoodsEntity goodsEntity);
+
+    PageResult findPage(Goods goods, Integer page, Integer rows);
+
+    GoodsEntity findOne(Long id);
+
+    void update(GoodsEntity goodsEntity);
+
+    void delete(Long[] ids);
+
+    void updateStatus(Long[] ids, String status);
+
+}
